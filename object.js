6cde50aede162a33cf91f73b10dd5f4c6d152a78
Md Sakib hossen <akibhossainsakib1040@gmail.com>
2025-09-16 19:12:31 +0600

    object

---
+++ object.js
@@ -0,0 +1,14 @@
+const friends = ["Alim", "Kolim", "Dalim", "Zalim"];
+
+const person = {
+  name: "Shakib Hossen",
+  age: 22,
+  friends: friends,
+};
+
+console.log(person.friends[0]);
+
+person.name = "Modhusudhon Roy";
+console.log(person);
+person.location = "Dhaka";
+console.log(person);
