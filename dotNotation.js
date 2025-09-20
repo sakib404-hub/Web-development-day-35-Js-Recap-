84adda3db06e1e9e78185010c23fdf254e745c83
Md Sakib hossen <akibhossainsakib1040@gmail.com>
2025-09-20 13:34:15 +0600

    dot notation in javascript

---
+++ dotNotation.js
@@ -0,0 +1,18 @@
+const person = {
+  name: "Hero Alom",
+  age: 10,
+  friends: ["Alim", "Kalim", "Dalim"],
+  salary: 100000,
+  10: "Secret Code",
+};
+
+const heroName = person["name"];
+console.log(heroName);
+
+const number = person["10"];
+console.log(number);
+
+/**
+ * ? Amra object er khetre kokhono kokhono dot er jaigai bracket use
+ * ? maximum time unusal case jegula ache se jonno bracket notation use kora hoise
+ *  */
