1292eeda1e8ab1fe06234e924157132ce39d468e
Md Sakib hossen <akibhossainsakib1040@gmail.com>
2025-09-20 15:30:13 +0600

    varibale types

---
+++ beforeReact/variable.js
@@ -0,0 +1,26 @@
+/**
+ * TODO: var
+ * ?Old way of  declearing the variable
+ * ? the var is function scoped therefore it is visible within the function only
+ * ? Var can be redecleared and updated
+ */
+
+var x = 4; //?Declearation
+var x = 10; //? Redeclearation
+console.log(x);
+
+/**
+ * TODO : Let
+ * ? Block Scoped thefore visible only inside the block
+ * ? Can be updated but can not be redecleared in the same scope
+ */
+
+let y = 5;
+y = 10;
+
+/**
+ * TODO : Const
+ * ? Block Scoped Therefore visible only within the curley Braces
+ * ? This const can not be redecleard or updated
+ * ? Must be assigned a value while declearing
+ */
